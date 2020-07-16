9345a879d9b000c14e36f3aad9e77b185f3aec56
Eugene Kim <eugkim1997@gmail.com>
2020-07-16 08:00:50 -0700

    added state functionality

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+      hovered: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+var GroceryListItems = (props) => (
+  <ul>
+    {props.groceries.map(item =>
+    <GroceryListItem item={item} />
+  )}
+  </ul>
+);
+
+
+ReactDOM.render(<GroceryListItems groceries={['Milk', 'Eggs', 'Bread', 'Soda', 'Beer']} />, document.getElementById("app"));
